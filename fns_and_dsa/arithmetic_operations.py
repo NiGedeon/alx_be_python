a5cab559d35aad580e27a8abbcb65231d1072d8c
NiGedeon <nigideon300@gmail.com>
2024-09-26 01:19:30 -0700

    the arithmetic_operations.py file is updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,16 @@
 def perform_operation(num1, num2, operation):
-        match(operation):
-            case "add":
-                result = num1 + num2
-            case "subtract":
-                result = num1 - num2
-            case "multiply":
-                result = num1 * num2
-            case "divide" :
-                result = num1 /num2 if num2 != 0 else "It's impossible to divide a number by zero." 
-            case _ :                                                                                                            result = "Invalid input"                                                                                return result
+        if (operation == "add"):
+            result = num1 + num2
+            print(result)
+        elif(operation == "subtract"):
+            result = num1 - num2
+            print(result)
+        elif(operation == "multiply"):
+            result = num1 * num2
+            print(result)
+        elif(operation == "divide") :
+            result = num1 /num2 if num2 != 0 else "It's impossible to divide a number by zero."
+            print(result)
+        else :
+            result = "Invalid input"
+            print(result)
