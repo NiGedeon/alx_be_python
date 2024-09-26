cc4ba19f9c06db484dfab6964853026f452f515f
NiGedeon <nigideon300@gmail.com>
2024-09-26 01:32:15 -0700

    Another modification

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,8 +9,12 @@ def perform_operation(num1, num2, operation):
             result = num1 * num2
             print(result)
         elif(operation == "divide") :
-            result = num1 /num2 if num2 != 0 else "It's impossible to divide a number by zero."
-            print(result)
+            if num2 == 0:
+                result = "Error: Division by zero is not possible"
+                print(result)
+            else:
+                result = num1 /num2 
+                print(result)
         else :
             result = "Invalid input"
             print(result)
