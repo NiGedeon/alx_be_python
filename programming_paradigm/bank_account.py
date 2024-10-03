43da476fa60839437c7db4157b5140a2d38b312f
NiGedeon <nigideon300@gmail.com>
2024-10-03 02:25:46 -0700

    bank_account. py file is added

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    
+    def __init__(self,account_balance = 0) :
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        self.amount = amount
+        self.account_balance = self.account_balance + self.amount
+        return self.account_balance
+    
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.account_balance >= self.amount:
+            self.account_balance = self.account_balance - self.amount
+            return True
+        else:
+
+            return False
+    
+    def display_balance(self):
+        print(f"The balance you are remaning with is {self.account_balance:.2f} ")
+
