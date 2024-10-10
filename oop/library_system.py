c8652ac884133bdc1b069c52633779341abb6b85
NiGedeon <nigideon300@gmail.com>
2024-10-10 02:22:10 -0700

    library_system.py is added

--- oop/library_system.py
+++ oop/library_system.py
@@ -9,7 +9,7 @@ class EBook(Book):
     def __init__(self, title, author,file_size):
         super().__init__(title, author)
         self.file_size = file_size
-     def __str__(self):
+    def __str__(self):
         return f"{self.title} by {self.author} {self.file_size}"
 
 
@@ -17,7 +17,7 @@ class PrintBook(Book):
     def __init__(self, title, author,page_count):
         super().__init__(title, author)
         self.page_count = page_count
-     def __str__(self):
+    def __str__(self):
          return f"{self.title} by {self.author} {self.page_count}"
 
 
